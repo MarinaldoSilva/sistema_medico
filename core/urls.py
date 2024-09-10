d2530cf0b401e12fcea3fe6b488b027e596e2fff
Marinaldo <marinaldo12@hotmail.com>
2024-09-10 09:42:27 -0300

    Projeto não inicia para teste

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+from apps.base import views as base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('base/', base_views.base_view, name='base'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
